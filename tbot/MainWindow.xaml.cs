9706e19921eb93989a017c5b341d1bebc45c06ef
xkings <xkingsgames@gmail.com>
2014-08-31 03:04:02 +0200

    Bot is able to read it's own tweets and feed them into console.

--- tbot/MainWindow.xaml.cs
+++ tbot/MainWindow.xaml.cs
@@ -1,17 +1,34 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using tbot.Annotations;
 using tbot.bot;
 
 namespace tbot{
-    /// <summary>
-    ///     Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window{
+    public partial class MainWindow : Window, INotifyPropertyChanged{
         private readonly ProfileManager profileManager;
+        private Bot bot;
 
         public MainWindow(){
             profileManager = new ProfileManager();
             DataContext = profileManager;
             InitializeComponent();
+            SetupConsole();
+        }
+
+        public ConsoleStream BotConsoleStream { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SetupConsole(){
+            BotConsoleStream = new ConsoleStream();
+            BotConsoleStream.OnConsoleFeed += feed =>{
+                BotConsole.AppendText(feed);
+                OnPropertyChanged("BotConsoleStream");
+            };
+            Console.SetOut(BotConsoleStream);
         }
 
         private void AddProfile(object sender, RoutedEventArgs e){
@@ -20,9 +37,33 @@ namespace tbot{
             newWindow.ShowDialog();
         }
 
-        private void StartBot(object sender, RoutedEventArgs e){
+        private async void StartBot(object sender, RoutedEventArgs e){
             var profile = (Profile) Profiles.SelectedItem;
-            // bot.Init(profile);
+
+            bot = new Bot();
+            await bot.Init(profile);
+            await bot.readMyTeets(3);
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null){
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class ConsoleStream : TextWriter{
+        public delegate void ConsoleFeedHandler(string feed);
+
+        public override Encoding Encoding{
+            get { return Encoding.UTF8; }
+        }
+
+        public event ConsoleFeedHandler OnConsoleFeed;
+
+        public override void Write(char value){
+            base.Write(value);
+            if (OnConsoleFeed != null) OnConsoleFeed(value.ToString());
         }
     }
 }
---
+++ tbot/bot/Bot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinqToTwitter;
+
+namespace tbot.bot{
+    internal class Bot{
+        private TwitterContext twitterCtx;
+
+        public async Task Init(Profile profile){
+            twitterCtx = new TwitterContext(new SingleUserAuthorizer{
+                CredentialStore = new SingleUserInMemoryCredentialStore{
+                    ConsumerKey = profile.ConsumerKey,
+                    ConsumerSecret = profile.ConsumerSecret,
+                    AccessToken = profile.AccessToken,
+                    AccessTokenSecret = profile.AccessTokenSecret
+                }
+            });
+        }
+
+        public async Task readMyTeets(int count){
+            List<Status> friendTweets = await (from tweet in twitterCtx.Status
+                where tweet.Type == StatusType.User  && tweet.Count == count
+                select tweet).ToListAsync();
+
+            if (friendTweets != null){
+                Console.WriteLine("Tweets: \n");
+                friendTweets.ForEach(tweet =>{
+                    if (tweet != null && tweet.User != null)
+                        Console.WriteLine(
+                            "User: " + tweet.User.Name +
+                            "\nTweet: " + tweet.Text +
+                            "\nTweet ID: " + tweet.ID + "\n");
+                });
+            }
+        }
+    }
+}
